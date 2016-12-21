0e40963052eb59da796948e90d9212fca0e234da
Eduardo Bruno <eduardo@bruno.etc.br>
2016-12-21 16:47:32 -0200

    failure branch E

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -16,7 +16,7 @@ public class GreeterTest {
     @Test
     public void testSayHelloParam() {
         Greeter subject = new Greeter();
-        assertEquals("Hello failure D", subject.sayHello("dummy"));
+        assertEquals("Hello failure E", subject.sayHello("dummy"));
     }
 
     @Test
